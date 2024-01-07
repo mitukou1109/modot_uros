16945afe217a87ede2429e93e4a4206649bc2c53
mitukou1109 <mitukou1109@gmail.com>
2024-01-07 11:52:11 +0900

    Init

---
+++ modot_uros.ino
@@ -0,0 +1,84 @@
+#include <micro_ros_arduino.h>
+#include <rcl/error_handling.h>
+#include <rcl/rcl.h>
+#include <rclc/executor.h>
+#include <rclc/rclc.h>
+#include <std_msgs/msg/bool.h>
+#include <stdio.h>
+
+static const uint8_t WING_RELAY_TRIGGER_PIN = 23;
+
+rcl_subscription_t subscriber;
+std_msgs__msg__Bool msg;
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+
+#define RCCHECK(fn)              \
+  {                              \
+    rcl_ret_t temp_rc = fn;      \
+    if ((temp_rc != RCL_RET_OK)) \
+    {                            \
+      error_loop();              \
+    }                            \
+  }
+#define RCSOFTCHECK(fn)          \
+  {                              \
+    rcl_ret_t temp_rc = fn;      \
+    if ((temp_rc != RCL_RET_OK)) \
+    {                            \
+    }                            \
+  }
+
+void error_loop()
+{
+  while (1)
+  {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(100);
+  }
+}
+
+void obstacle_detected_callback(const void* msgin)
+{
+  const auto* msg = static_cast<const std_msgs__msg__Bool*>(msgin);
+  digitalWrite(WING_RELAY_TRIGGER_PIN, msg->data ? HIGH : LOW);
+}
+
+void setup()
+{
+  set_microros_transports();
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  pinMode(WING_RELAY_TRIGGER_PIN, OUTPUT);
+  digitalWrite(WING_RELAY_TRIGGER_PIN, LOW);
+
+  delay(2000);
+
+  allocator = rcl_get_default_allocator();
+
+  // create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "modot_uros", "", &support));
+
+  // create subscriber
+  RCCHECK(rclc_subscription_init_default(
+      &subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Bool),
+      "obstacle_detector/detected"));
+
+  // create executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_subscription(
+      &executor, &subscriber, &msg, &obstacle_detected_callback, ON_NEW_DATA));
+}
+
+void loop()
+{
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+}
